b79b6b060d9cf38cd16b4431d2cb411538d5bc9c
Juana Vicente <juanivicentee@gmail.com>
2026-06-18 10:23:24 -0300

    Implementación de Persistencia

---
+++ Dsw2026Ej15/Dsw2026Ej15.Data/PersistenceInMemory.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.Json;
+using Dsw2026Ej15.Domain.Entities;
+using Dsw2026Ej15.Domain.Interfaces;
+
+namespace Dsw2026Ej15.Data;
+
+public class PersistenceInMemory : IPersistence
+{
+    private List<Doctor> doctors;
+
+    private List<Speciality> specialities;
+
+    public PersistenceInMemory()
+    {
+        doctors = new List<Doctor>();
+
+        specialities = LoadSpecialities();
+    }
+
+    private List<Speciality> LoadSpecialities()
+    {
+        string json = File.ReadAllText("specialities.json");
+
+        return JsonSerializer.Deserialize<List<Speciality>>(json)
+               ?? new List<Speciality>();
+    }
+
+    public List<Doctor> GetDoctors()
+    {
+        return doctors;
+    }
+
+    public Doctor? GetDoctorById(Guid id)
+    {
+        return doctors.FirstOrDefault(d => d.Id == id);
+    }
+
+    public void AddDoctor(Doctor doctor)
+    {
+        doctors.Add(doctor);
+    }
+
+    public List<Speciality> GetSpecialities()
+    {
+        return specialities;
+    }
+
+    public Speciality? GetSpecialityById(Guid id)
+    {
+        return specialities.FirstOrDefault(s => s.Id == id);
+    }
+}
---
+++ Dsw2026Ej15/Dsw2026Ej15.Domain/Interfaces/IPersistence.cs
@@ -0,0 +1,16 @@
+﻿using Dsw2026Ej15.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej15.Domain.Interfaces
+{
+    public interface IPersistence
+    {
+        List<Doctor> GetDoctors();
+        Doctor GetDoctorById(Guid Id);
+        void AddDoctor(Doctor doctor);
+        List<Speciality> GetSpecialities();
+        Speciality GetSpecialityById(Guid id);
+    }
+}
