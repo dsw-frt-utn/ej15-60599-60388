0964cd2bf6673719506c256717a6d323b35f128d
Iván Chirino <civanchirino@gmail.com>
2026-06-18 11:03:56 -0300

    Desarrollo de SpecialityDto

--- Dsw2026Ej15/Dsw2026Ej15.Data/Dtos/SpecialityDto.cs
+++ Dsw2026Ej15/Dsw2026Ej15.Data/Dtos/SpecialityDto.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Dsw2026Ej15.Data.Dtos
-{
-    internal class SpecialityDto
-    {
-    }
-}
+namespace Dsw2026Ej15.Data.Dtos;
+
+internal record SpecialityDto(Guid Id, string Name, string Description);
+    
+    
+
